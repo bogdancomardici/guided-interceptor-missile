7962666fa7728508fb0cb84b9796f985ad60aa91
bogdancomardici <115560133+bogdancomardici@users.noreply.github.com>
2022-10-12 23:45:18 +0300

    added more comments

--- rocket.py
+++ rocket.py
@@ -5,7 +5,7 @@ import math
 s = turtle.Screen()
 s.setup(width = 550, height = 550)
 
-
+# set up both missiles
 incoming_missile = turtle.Turtle()
 incoming_missile.hideturtle()
 
@@ -17,12 +17,12 @@ incoming_missile.penup()
 interceptor.color("blue")
 interceptor.penup()
 
-
+# incoming missile in left corner
 incoming_missile.goto(-250, -250)
 incoming_missile.showturtle()
 incoming_missile.left(45)
 
-
+# interceptor missile in bottom center
 interceptor.goto(0, -250)
 interceptor.showturtle()
 interceptor.left(90)
@@ -49,6 +49,9 @@ while True:
             delta = dx / dy
             heading =math.degrees(math.atan(delta))
             interceptor.setheading(heading + 90)
+
+        # we get the interceptor heading by calculating the arctangent in the rectangle formed by the x and y coordinates
+        # then convert it to degrees and offest it from de horizontal for each case
 
         # target is up and right
 
@@ -82,8 +85,11 @@ while True:
 
         interceptor.forward(3)
 
+        # when the missiles colide
+
         if((interceptor.distance(incoming_missile)) < 3):
             
+            # new turtle to draw the explosion
             incoming_missile.hideturtle()
             interceptor.hideturtle()
             explosion = turtle.Turtle()
@@ -94,6 +100,7 @@ while True:
             explosion.goto(incoming_missile.pos())
             explosion.pendown()
 
+            #draw explosion
             for i in range(20):
                 angle = random.randint(0, 45)
                 distance = random.randint(0 ,30)
